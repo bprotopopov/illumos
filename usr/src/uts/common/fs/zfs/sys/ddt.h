7090c8d3e40454a99fad4e0470c15d9b942f1e47
Matthew Ahrens <mahrens@delphix.com>
2015-02-26 22:09:35 -0800

    cleanup

--- usr/src/uts/common/fs/zfs/sys/ddt.h
+++ usr/src/uts/common/fs/zfs/sys/ddt.h
@@ -35,6 +35,8 @@
 extern "C" {
 #endif
 
+struct abd;
+
 /*
  * On-disk DDT formats, in the desired search order (newest version first).
  */
@@ -108,7 +110,7 @@ struct ddt_entry {
 	ddt_key_t	dde_key;
 	ddt_phys_t	dde_phys[DDT_PHYS_TYPES];
 	zio_t		*dde_lead_zio[DDT_PHYS_TYPES];
-	abd_t		*dde_repair_data;
+	struct abd	*dde_repair_data;
 	enum ddt_type	dde_type;
 	enum ddt_class	dde_class;
 	uint8_t		dde_loading;
